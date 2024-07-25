2c58d8ac64520a2a4738d15dbaa5ceea9d8a0bfe
chamod udara <uchamod52@gmail.com>
2024-07-25 22:35:06 +0530

    add bottom navigation bar

---
+++ lib/pages/addpostpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddPostPage extends StatefulWidget {
+  const AddPostPage({super.key});
+
+  @override
+  State<AddPostPage> createState() => _AddPostPageState();
+}
+
+class _AddPostPageState extends State<AddPostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Center(
+        child: Text("this is add post page"),
+      ),
+    );
+  }
+}
--- lib/responsive/mobilescreenLayout.dart
+++ lib/responsive/mobilescreenLayout.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user_model.dart';
+import 'package:instagram_clone/pages/addpostpage.dart';
+import 'package:instagram_clone/pages/homepage.dart';
+import 'package:instagram_clone/pages/notifiypage.dart';
+import 'package:instagram_clone/pages/profilepage.dart';
+import 'package:instagram_clone/pages/serchpage.dart';
 import 'package:instagram_clone/provider/user_provider.dart';
+import 'package:instagram_clone/util/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenlayout extends StatefulWidget {
@@ -11,8 +18,23 @@ class MobileScreenlayout extends StatefulWidget {
 }
 
 class _MobileScreenlayoutState extends State<MobileScreenlayout> {
+  PageController _pageController = PageController();
+  int _page = 0;
   @override
- 
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void pageChange(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void onIconTap(int page) {
+    _pageController.jumpToPage(page);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +42,54 @@ class _MobileScreenlayoutState extends State<MobileScreenlayout> {
     UserModel user = Provider.of<UserProvider>(context).getCurrentUser;
 
     return Scaffold(
-      body: Center(
-        child: Text(
-          user.username,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: pageChange,
+          children: const [
+            Homepage(),
+            SerchPage(),
+            AddPostPage(),
+            Notifiypage(),
+            ProfilePage(),
+          ],
         ),
-      ),
-    );
+        bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: mobileSearchColor,
+          onTap: onIconTap,
+          currentIndex: _page,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.home,
+              size: _page == 0 ? 34 : 30,
+              color: _page == 0 ? primaryColor : primaryColor.withOpacity(0.7),
+            )),
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.search,
+              size: _page == 1 ? 34 : 30,
+              color: _page == 1 ? primaryColor : primaryColor.withOpacity(0.7),
+            )),
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.add_circle,
+              size: _page == 2 ? 34 : 30,
+              color: _page == 2 ? primaryColor : primaryColor.withOpacity(0.7),
+            )),
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.notifications,
+              size: _page == 3 ? 34 : 30,
+              color: _page == 3 ? primaryColor : primaryColor.withOpacity(0.7),
+            )),
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.person,
+              size: _page == 4 ? 34 : 30,
+              color: _page == 4 ? primaryColor : primaryColor.withOpacity(0.7),
+            )),
+          ],
+        ));
   }
 }
  // String username = "";
